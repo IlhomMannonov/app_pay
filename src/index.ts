be7e02bf946518b39f5f072c90a9e2bc1287690f
unknown <ilhomMannonov>
2024-08-13 10:42:17 +0500

    ,

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import http from 'http';
 import app from './app';
 import config from './config/config';
+import cors from "cors";
 
 const index = http.createServer(app);
+
+app.use(cors());
 
 index.listen(config.port, () => {
     console.log(`Server running on port ${config.port}`);
